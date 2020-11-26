2e18f9e064c7adb92e6d9220bcd7a023abaa346e
henansong <he.nansong@kenes.com.cn>
2020-11-26 16:28:13 +0900

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        the Dog named Money
+    <div class="nav-wrapper">
+        <div class="content">
+            the Dog named Money
+        </div>
         <Nav/>
     </div>
 </template>
@@ -12,5 +14,18 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+        border: 1px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        overflow: auto;
+        /*
+        content 内容过多会自己变成滚动，
+        这样，即使 content 里内容很多，但其高度也始终 = 100vh - nav 的高度
+        */
+        flex-grow: 1;
+    }
 </style>
