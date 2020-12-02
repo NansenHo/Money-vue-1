53faceb1446a3e279c76a0d2a6c7151cd2b67f27
henansong <he.nansong@kenes.com.cn>
2020-12-02 14:06:20 +0900

    基本完成 NumberPad 组件

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,29 +1,62 @@
 <template>
     <div class="number-pad">
-        <div class="number-pad-output">100</div>
+        <div class="number-pad-output">{{output || '0.00'}}</div>
+        <!-- 给一个保底值，不让输入框塌下去 -->
         <div class="number-pad-buttons">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>删除</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>清空</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button class="ok">ok</button>
-            <button class="zero">0</button>
-            <button>.</button>
+            <button @click="inputContent">1</button>
+            <button @click="inputContent">2</button>
+            <button @click="inputContent">3</button>
+            <button @click="remove">删除</button>
+            <button @click="inputContent">4</button>
+            <button @click="inputContent">5</button>
+            <button @click="inputContent">6</button>
+            <button @click="clear">清空</button>
+            <button @click="inputContent">7</button>
+            <button @click="inputContent">8</button>
+            <button @click="inputContent">9</button>
+            <button @click="ok" class="ok">完成</button>
+            <button class="zero" @click="inputContent">0</button>
+            <button @click="inputContent">.</button>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name: 'NumberPad'
-    };
+    import Vue from 'vue';
+    import {Component, Prop} from 'vue-property-decorator';
+
+    @Component
+    export default class NumberPad extends Vue {
+        output: string = '0.00';
+
+        inputContent(event: MouseEvent) {
+            const button = (event.target as HTMLButtonElement);
+            const input = button.textContent as string;
+            //OR const input = button.textContent! // 感叹号就是排除空，其他保留的意思
+            if(this.output.length === 8){return;}
+            if (this.output === '0.00') {
+                if ('0123456789'.indexOf(input) >= 0) {
+                    this.output = input;
+                } else {
+                    this.output = '0.';
+                }
+                return;
+            }
+            if(this.output.indexOf('.') >= 0 && input === '.'){return;}
+            this.output += input;
+        }
+
+        remove(){
+            this.output = this.output.slice(0, -1)
+        }
+        clear(){
+            this.output = this.output.substr(0,-1)
+        }
+        ok(){
+
+        }
+
+    }
 </script>
 
 <style lang="scss" scoped>
