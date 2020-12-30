b39c9266001b37996a7d425a28aa5da0ca65f1ec
henansong <github@nansenho.com>
2020-12-30 12:19:40 +0900

    在 Money.vue 里使用 Tabs

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -6,7 +6,9 @@
              @update:value="onUpdateRemarks"
     />
     <Label/>
-    <Types :value.sync="record.type"/>
+    <Tabs class-prefix="types"
+          :data-source="recordTypeList"
+          :value.sync="record.type" />
   </layout>
 </template>
 
@@ -18,17 +20,21 @@ import Remarks from '@/components/Money/Remarks.vue';
 import Label from '@/components/Money/Label.vue';
 import {Component} from 'vue-property-decorator';
 import store from '@/store/index';
+import recordTypeList from '@/constants/recordTypeList';
+import Tabs from '@/components/Tabs.vue';
 // import store2 from '@/store/index2';
 //
 // console.log(store === store2);
 
 @Component({
-  components: {Label, Remarks, Types, NumberPad},
+  components: {Label, Remarks, Types, NumberPad, Tabs},
 })
 export default class Money extends Vue {
   get recordList() {
     return this.$store.state.recordList;
   }
+
+  recordTypeList = recordTypeList;
 
   record: RecordItem = {labels: [], remarks: '', type: '-', amount: 0};
 
