5fe9f127311d5a9bebf4b54ff3faf99844cc3c1f
henansong <github@nansenho.com>
2020-12-31 12:31:35 +0900

    完成列表的 CSS

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -55,7 +55,7 @@ export default class Money extends Vue {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .layout-content {
   display: flex;
   flex-direction: column-reverse;
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,18 +2,50 @@
   <layout>
     <Tabs class-prefix="types" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-    <div>
-      type: {{ type }}
-      <br/>
-      interval: {{ interval }}
-    </div>
-    <div>
-      <ol>
-        <li v-for="item in result" :key="item.id">{{ item }}</li>
-      </ol>
-    </div>
+    <ol>
+      <li v-for="(group, index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id"
+              class="record"
+          >
+            <span>{{ labelString(item.labels) }}</span>
+            <span class="notes">{{ item.remarks }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </layout>
 </template>
+
+<style scoped lang="scss">
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.title {
+  @extend %item;
+}
+
+.record {
+  @extend %item;
+  background: white;
+}
+
+.notes {
+  margin-right: 16px;
+  margin-left: 16px;
+  color: #999;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue';
@@ -26,20 +58,30 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs},
 })
 export default class Statistics extends Vue {
+  labelString(labels: Label[]) {
+    return labels.length === 0 ? '无' : labels.join(',');
+  }
+
   get recordList() {
     return (this.$store.state as RootState).recordList;
   }
 
   get result() {
     const {recordList} = this;
-    const hashTable = {};
+    type HashTableValue = { title: string, items: RecordList[] }
+
+    const hashTable: { [key: string]: HashTableValue } = {};
     for (let i = 0; i < recordList.length; i++) {
-      console.log(recordList[i].createdAt);
+      const [date, time] = recordList[i].createdAt!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items: []};
+
+      hashTable[date].items.push(recordList[i]);
     }
-    return [];
+    // console.log(hashTable);
+    return hashTable;
   }
 
-  mounted() {
+  beforeCreate() {
     this.$store.commit('fetchRecords');
   }
 
@@ -67,7 +109,7 @@ export default class Statistics extends Vue {
   }
 
   .interval-tabs-item {
-    height: 44px;
+    height: 48px;
   }
 }
 </style>
