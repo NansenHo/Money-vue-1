ed3e9d2df867c9ef4ecade238304588b43517907
henansong <github@nansenho.com>
2020-12-31 15:47:54 +0900

    完成统计页面上的显示时间问题 - ISO8601 & dayjs

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,8 +3,8 @@
     <Tabs class-prefix="types" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <ol>
-      <li v-for="(group, index) in result" :key="index">
-        <h3 class="title">{{ group.title }}</h3>
+      <li v-for="group in result" :key="group.title">
+        <h3 class="title">{{ beautify(group.title) }}</h3>
         <ol>
           <li v-for="item in group.items" :key="item.id"
               class="record"
@@ -19,41 +19,13 @@
   </layout>
 </template>
 
-<style scoped lang="scss">
-%item {
-  padding: 8px 16px;
-  line-height: 24px;
-  display: flex;
-  justify-content: space-between;
-  align-content: center;
-}
-
-.title {
-  @extend %item;
-}
-
-.record {
-  @extend %item;
-  background: white;
-}
-
-.notes {
-  flex: 1;
-  margin-right: 16px;
-  margin-left: 16px;
-  color: #999;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
-</style>
-
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import Tabs from '@/components/Tabs.vue';
 import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
+import dayjs from 'dayjs';
 
 @Component({
   components: {Tabs},
@@ -61,6 +33,25 @@ import recordTypeList from '@/constants/recordTypeList';
 export default class Statistics extends Vue {
   labelString(labels: Label[]) {
     return labels.length === 0 ? '无' : labels.join(',');
+  }
+
+  beautify(string: string) {
+    const day = dayjs(string);
+    const today = dayjs();
+    console.log(today.subtract(1, 'day').toDate());
+    // console.log(today.subtract(1, 'day'));
+    if (day.isSame(today, 'day')) {
+      return '今天';
+    } else if (day.isSame(today.subtract(1, 'day'), 'day')) {
+      console.log('hi');
+      return '昨天';
+    } else if (day.isSame(today.subtract(2, 'day'), 'day')) {
+      return '前天';
+    } else if (day.isSame(today, 'year')) {
+      return day.format('M月D日');
+    } else {
+      return day.format('YYYY年M月D日');
+    }
   }
 
   get recordList() {
@@ -112,5 +103,32 @@ export default class Statistics extends Vue {
   .interval-tabs-item {
     height: 48px;
   }
+}
+
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.title {
+  @extend %item;
+}
+
+.record {
+  @extend %item;
+  background: white;
+}
+
+.notes {
+  flex: 1;
+  margin-right: 16px;
+  margin-left: 16px;
+  color: #999;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 </style>
